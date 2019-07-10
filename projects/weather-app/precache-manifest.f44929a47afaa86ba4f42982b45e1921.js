78379065b97b96d71bf15672216df648ccce74b6
Pavel <39166538+code-pav@users.noreply.github.com>
2019-07-10 21:16:48 +0300

    first stable PC version

---
+++ projects/weather-app/precache-manifest.f44929a47afaa86ba4f42982b45e1921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34f8d7b9f4e41eb527a96578ea66575",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a67f6480a330055f848b",
+    "url": "./static/css/main.d55e0648.chunk.css"
+  },
+  {
+    "revision": "6284665f5971701d15c3",
+    "url": "./static/js/2.fe462174.chunk.js"
+  },
+  {
+    "revision": "a67f6480a330055f848b",
+    "url": "./static/js/main.ebeb9bbd.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "2596f26a1ba1c1dda0b3454b52eb5a97",
+    "url": "./static/media/bg.2596f26a.jpg"
+  }
+]);
